c412a779e26daf0e2ae15109cb95bb6644262537
Epsi <45441683+EpsiRho@users.noreply.github.com>
2023-01-15 23:19:41 -0600

    yes

--- Extras/script.js
+++ Extras/script.js
@@ -5,3 +5,15 @@ from 'https://unpkg.com/@fluentui/web-components';
  */
 export const FluentDesignSystem = provideFluentDesignSystem().register(allComponents);
 baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
+
+function sendDateTime() {
+    const date = new Date(document.getElementById("datepicker").value + "T" + document.getElementById("timepicker").value);
+    const currentTime = date.toUTCString();
+    const socket = new WebSocket('ws://71.146.161.50:12222');
+
+    socket.onopen = function() {
+        socket.send(currentTime);
+    };
+}
+
+document.getElementById("submit").addEventListener("click", sendDateTime);
