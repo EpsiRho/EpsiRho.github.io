b1af0ccedca83c40f613c8f24d69f536cc7a5d5c
Epsi <45441683+EpsiRho@users.noreply.github.com>
2023-01-15 23:07:44 -0600

    ok

--- Extras/script.js
+++ Extras/script.js
@@ -5,20 +5,3 @@ from 'https://unpkg.com/@fluentui/web-components';
  */
 export const FluentDesignSystem = provideFluentDesignSystem().register(allComponents);
 baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
-
-
-// JavaScript function to send the date and time
-function sendDateTime() {
-    const date = new Date(document.getElementById("datepicker").value + "T" + document.getElementById("timepicker").value);
-    const currentTime = date.toUTCString();
-    const net = require('net');
-
-    const client = new net.Socket();
-    client.connect(12222, '71.146.161.50', function() {
-        const date = new Date();
-        const currentTime = date.toUTCString();
-        client.write(currentTime);
-    });
-}
-// add event listener to the submit button
-document.getElementById("submit").addEventListener("click", sendDateTime);
