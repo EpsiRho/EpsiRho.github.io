b0db75a59db64614ada703f14e30b6ad9825c7c4
Epsi <45441683+EpsiRho@users.noreply.github.com>
2023-01-16 01:12:00 -0600

    Update script.js

--- Extras/script.js
+++ Extras/script.js
@@ -9,11 +9,16 @@ baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
 function sendDateTime() {
     const date = new Date(document.getElementById("datepicker").value + "T" + document.getElementById("timepicker").value);
     const currentTime = date.toUTCString();
-    const socket = new WebSocket('wss://71.146.161.50:12222');
-
-    socket.onopen = function() {
-        socket.send(currentTime);
-    };
+    //const socket = new WebSocket('wss://71.146.161.50:12222');
+    fetch("http://71.146.161.50:12222", {
+            method: "POST",
+            body: currentTime,
+            //headers: { "Content-Type": "application/json" }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Server response:", data);
+        });
 }
 
 document.getElementById("submit").addEventListener("click", sendDateTime);
