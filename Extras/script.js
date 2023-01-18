8f5e523b1314e7d7ccb6675dc890ded71da66297
Epsi <45441683+EpsiRho@users.noreply.github.com>
2023-01-17 23:28:52 -0600

    ugh

--- Extras/script.js
+++ Extras/script.js
@@ -8,17 +8,17 @@ baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
 
 function sendDateTime() {
     const date = new Date(document.getElementById("datepicker").value + "T" + document.getElementById("timepicker").value);
-    const currentTime = date.toLocaleDateString();
+    const currentTime = date.toLocaleString();
     const currentTimeUTC = date.toUTCString();
     const encoder = new TextEncoder();
     const encodedBytes = encoder.encode(currentTime + currentTimeUTC);
+    console.log(currentTime + "|" + currentTimeUTC);
     //const socket = new WebSocket('wss://71.146.161.50:12222');
-    fetch("http://71.146.161.50:12222", {
+    fetch("http://71.146.161.50:5238/api/EntryLogger", {
             method: "POST",
-            body: encodedBytes,
+            body: currentTime + "|" + currentTimeUTC,
             //headers: { "Content-Type": "application/json" }
         })
-        .then(response => response.json())
         .then(data => {
             console.log("Server response:", data);
         });
