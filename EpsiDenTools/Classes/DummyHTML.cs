3a6d19b6831db214e1c20154bd600b8521803561
Epsi <epsirho@gmail.com>
2024-12-16 06:22:24 -0600

    fixes to mobile

--- EpsiDenTools/Classes/DummyHTML.cs
+++ EpsiDenTools/Classes/DummyHTML.cs
@@ -17,6 +17,7 @@ namespace EpsiDenTools.Classes
     <meta charset=""UTF-8"">
     <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
     <title>{{POSTTABTITLE}}</title>
+    <link rel=""icon"" type=""image/x-icon"" href=""/images/favicon.ico"">
     <link rel=""stylesheet"" href=""/styles/main.css"">
     <link rel=""stylesheet"" href=""/styles/posts.css"">
 </head>
